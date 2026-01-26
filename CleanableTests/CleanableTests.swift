042102c9244dd75c6474010c39e11b5aeecc76a2
lerodic <daniel@rodev.net>
2026-01-26 10:14:45 +0100

    Initial commit

---
+++ CleanableTests/CleanableTests.swift
@@ -0,0 +1,8 @@
+@testable import Cleanable
+import Testing
+
+struct CleanableTests {
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+}
