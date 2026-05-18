4dc641f3de31a63d40b43b366922216398e7870a
lerodic <danielrodich@outlook.de>
2026-05-18 11:59:29 +0200

    feat(app): Update 'TrackpadMonitorDelegate' protocol
    Force concrete implementations to expose status item frame + whether status item is interactive

--- Cleanable/Delegates/TrackpadMonitorDelegate.swift
+++ Cleanable/Delegates/TrackpadMonitorDelegate.swift
@@ -2,4 +2,6 @@ import AppKit
 
 protocol TrackpadMonitorDelegate: AnyObject {
     func trackpadMonitor(_ monitor: TrackpadMonitor, shouldBlockEvent event: NSEvent) -> Bool
+    func trackpadMonitorStatusItemFrame(_ monitor: TrackpadMonitor) -> CGRect?
+    func trackpadMonitorIsStatusItemInteractive(_ monitor: TrackpadMonitor) -> Bool
 }
