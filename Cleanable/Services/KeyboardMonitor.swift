0dbcd72c5965d9dac93fe8ba698907c4e31be194
lerodic <danielrodich@outlook.de>
2026-06-24 13:48:30 +0200

    feat(app): Allow keyboard monitor to block all Fn keys
    Check for audio-related Fn keys separately

--- Cleanable/Services/KeyboardMonitor.swift
+++ Cleanable/Services/KeyboardMonitor.swift
@@ -2,6 +2,17 @@ import AppKit
 import Foundation
 
 class KeyboardMonitor {
+    private enum MediaKeyType: Int {
+        case volumeUp = 0
+        case volumeDown = 1
+        case mute = 7
+        case play = 16
+        case next = 17
+        case previous = 18
+        case fastForward = 19
+        case rewind = 20
+    }
+    
     weak var delegate: KeyboardMonitorDelegate?
     
     private static var eventMask: CGEventMask {
@@ -89,27 +100,87 @@ class KeyboardMonitor {
     
     @discardableResult
     func handle(event: CGEvent, ofType type: CGEventType) -> Unmanaged<CGEvent>? {
-        guard type == .keyDown else {
+        if shouldBlockKeyPress(type, event) {
+            return nil
+        }
+
+        guard shouldBlockEvent(event) else {
             return Unmanaged.passUnretained(event)
         }
-        
+
+        if isAudioKey(type) {
+            return handleAudioKey(event)
+        }
+
+        return nil
+    }
+    
+    private func shouldBlockKeyPress(_ type: CGEventType, _ event: CGEvent) -> Bool {
+        return type == .keyDown && handleKeyPress(event)
+    }
+    
+    private func handleKeyPress(_ event: CGEvent) -> Bool {
         let nsEvent = NSEvent(cgEvent: event)
-        
+
         if let nsEvent, currentShortcut.matches(event: nsEvent) {
             DispatchQueue.main.async { [weak self] in
                 guard let self else { return }
-                
                 self.delegate?.keyboardMonitor(self, didDetectShortcut: self.currentShortcut)
             }
             
-            return nil
+            return true
         }
         
-        if let nsEvent, delegate?.keyboardMonitor(self, shouldBlockEvent: nsEvent) == true {
-            return nil
+        return false
+    }
+    
+    private func shouldBlockEvent(_ event: CGEvent) -> Bool {
+        guard let nsEvent = NSEvent(cgEvent: event) else { return false }
+            
+        return delegate?.keyboardMonitor(self, shouldBlockEvent: nsEvent) == true
+    }
+    
+    private func isAudioKey(_ type: CGEventType) -> Bool {
+        return type.rawValue == 14
+    }
+    
+    private func handleAudioKey(_ event: CGEvent) -> Unmanaged<CGEvent>? {
+        guard let nsEvent = NSEvent(cgEvent: event), nsEvent.subtype.rawValue == 8 else {
+            return Unmanaged.passUnretained(event)
         }
+
+        let keyType = (nsEvent.data1 & 0xFFFF0000) >> 16
         
-        return Unmanaged.passUnretained(event)
+        return MediaKeyType(rawValue: keyType) != nil
+            ? nil
+            : Unmanaged.passUnretained(event)
+    }
+    
+    func setFnKeysAsStandard(_ enabled: Bool) {
+        writeFnKeyState(enabled)
+        readFnKeyState()
+    }
+    
+    private func writeFnKeyState(_ enabled: Bool) {
+        let value = enabled ? "1" : "0"
+        let process = createFnStateProcess("write", "com.apple.keyboard.fnState", value)
+        
+        try? process.run()
+        process.waitUntilExit()
+    }
+    
+    private func readFnKeyState() {
+        let notifyProcess = createFnStateProcess("read", "com.apple.keyboard.fnState")
+            
+        try? notifyProcess.run()
+    }
+    
+    private func createFnStateProcess(_ arguments: String...) -> Process {
+        let process = Process()
+        process.executableURL = URL(fileURLWithPath: "/usr/bin/defaults")
+        process.arguments = arguments
+        
+        return process
     }
     
     private func stopMonitoring() {
